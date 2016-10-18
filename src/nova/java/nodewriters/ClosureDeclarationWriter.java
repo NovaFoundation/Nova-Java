633da7dd4dd9d004521a28b8c312eeb30a018040
Braden Steffaniak <BradenSteffaniak@gmail.com>
2016-10-17 19:10:40 -0700

    Implemented ClosureDeclarationWriter.

--- src/nova/java/nodewriters/ClosureDeclarationWriter.java
+++ src/nova/java/nodewriters/ClosureDeclarationWriter.java
@@ -5,6 +5,35 @@ import net.fathomsoft.nova.tree.*;
 public abstract class ClosureDeclarationWriter extends ParameterWriter
 {
 	public abstract ClosureDeclaration node();
-	
-	
+
+	@Override
+	public StringBuilder writeExpression(StringBuilder builder)
+	{
+		if (node().getType() == null)
+		{
+			builder.append("NovaUtilities.Consumer");
+		}
+		else
+		{
+			builder.append("NovaUtilities.Function");
+		}
+		
+		builder.append(node().getParameterList().getNumParameters());
+		
+		builder.append("<");
+		
+		getWriter(node().getParameterList()).write(builder, false);
+		
+		if (node().getType() != null)
+		{
+			if (node().getParameterList().getNumParameters() > 0)
+			{
+				builder.append(", ");
+			}
+
+			builder.append(node().isPrimitive() ? node().getTypeClass().getName() : node().getType());
+		}
+		
+		return builder.append("> ").append(getWriter(node()).writeName());
+	}
 }
